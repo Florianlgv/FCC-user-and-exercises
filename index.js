8db4a51611f9584dbda0eea12492bff95e1b6e1e
Florianlgv <florianlangevine37@gmail.com>
2023-09-22 12:28:31 +0200

    feat: added schema and users route

--- index.js
+++ index.js
@@ -11,10 +11,52 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+let userSchema = new mongoose.Schema({
+  username: {
+        type: String,
+        required: true,
+        unique: true
+  }
+});
+const User = mongoose.model('User', userSchema);
+
+let exerciceSchema = new mongoose.Schema({
+  userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+  },
+  description: {
+      type: String,
+      required: true
+  },
+  duration: {
+      type: Number,
+      required: true
+  },
+  date: String,
+  dateD: Date
+});
+const Exercise = mongoose.model('Exercice', exerciceSchema);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  const newUser = new User({username: username});
+  newUser.save();
+  res.json({ username: username, _id: newUser._id})
+});
+
+app.get('/api/users', (req, res) => {
+  User.find()
+    .then((users) => {
+    res.json(users)
+    })
+    .catch((err) => console.log(err))
+});
 
 
 
