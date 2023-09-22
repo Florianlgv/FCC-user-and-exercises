a2cc652e1ac57ffee6e3d5ccfbc621bffc6f5e96
Florianlgv <florianlangevine37@gmail.com>
2023-09-22 12:27:13 +0200

    feat: db config, middleware

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
+const connectDB = require("./config/db");
 require('dotenv').config()
 
-app.use(cors())
-app.use(express.static('public'))
+connectDB();
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
